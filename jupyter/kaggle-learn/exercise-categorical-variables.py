fdf03fe9ba223280bd42318b560bfbdcb766c9cd
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2019-11-03 23:00:38 -0200

    melhorias

--- jupyter/kaggle-learn/exercise-categorical-variables.py
+++ jupyter/kaggle-learn/exercise-categorical-variables.py
@@ -15,33 +15,30 @@ y = X.SalePrice
 cols_with_missing = list(
   set([col for col in X.columns if X[col].isnull().any()]) |
   set([col for col in X_test.columns if X_test[col].isnull().any()]))
-print('Columns with missing values:\n', cols_with_missing)
+print('\nColumns with missing values:\n', cols_with_missing)
 
-missing_val_count_by_column = (X.isnull().sum()).to_frame()
+missing_val_count_by_column = X.isnull().sum()
 print('\nMissing values per column:\n', missing_val_count_by_column[missing_val_count_by_column > 0])
 
-#TODO: calcular percentual de valores faltantes por coluna
 total_rows_count = X.shape[0] + X_test.shape[0]
-
-#TODO: remover colunas com valores faltantes em < 80% dos casos
-#TODO: imputar valores faltantes nas colunas com >= 80% dos casos
-
-threshold = 0.8
-cols_missing_drop = ...
-cols_missing_impute = set(cols_with_missing) - set(cols_missing_drop)
-
+threshold = 0.9 # 90%
+cols_missing_drop = (missing_val_count_by_column[
+    missing_val_count_by_column > total_rows_count * (1 - threshold)]).index.values
+cols_missing_impute = list(set(cols_with_missing) - set(cols_missing_drop))
+print('\nColumns to be dropped:\n', cols_missing_drop)
+print('\nColumns to be imputed:\n', cols_missing_impute)
 ####################################################################################################
 
-X.drop(cols_with_missing, axis=1, inplace=True)
-X_test.drop(cols_with_missing, axis=1, inplace=True)
+X.drop(cols_missing_drop, axis=1, inplace=True)
+X_test.drop(cols_missing_drop, axis=1, inplace=True)
 
 ####################################################################################################
 
 from sklearn.impute import SimpleImputer
 
-my_imputer = SimpleImputer(strategy='most_frequent')
-imputed_X = pd.DataFrame(my_imputer.fit_transform(X))
-imputed_X_test = pd.DataFrame(my_imputer.transform(X_test))
+imputer = SimpleImputer(strategy='most_frequent')
+imputed_X = pd.DataFrame(imputer.fit_transform(X))
+imputed_X_test = pd.DataFrame(imputer.transform(X_test))
 
 imputed_X.columns = X.columns
 imputed_X_test.columns = X_test.columns
@@ -94,8 +91,8 @@ low_cardinality_cols = [col for col in object_cols if X[col].nunique() < 10]
 high_cardinality_cols = list(set(object_cols) - set(low_cardinality_cols))
 #high_cardinality_cols = [k for k, v in d.items() if v > 10]
 
-print('Categorical columns that will be one-hot encoded:', low_cardinality_cols)
-print('\nCategorical columns that will be dropped from the dataset:', high_cardinality_cols)
+print('\nCategorical columns that will be one-hot encoded:\n', low_cardinality_cols)
+print('\nCategorical columns that will be dropped from the dataset:\n', high_cardinality_cols)
 
 ####################################################################################################
 
