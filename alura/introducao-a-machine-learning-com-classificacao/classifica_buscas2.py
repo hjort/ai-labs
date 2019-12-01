ec60e5661b592ec2d80a8581fabbb9ef10321144
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2019-12-01 00:11:23 -0200

    aula 4

---
+++ alura/introducao-a-machine-learning-com-classificacao/classifica_buscas2.py
@@ -0,0 +1,42 @@
+import pandas as pd
+df = pd.read_csv('buscas.csv')
+X_df = df[['home', 'busca', 'logado']]
+Y_df = df['comprou']
+
+Xdummies_df = pd.get_dummies(X_df)
+Ydummies_df = Y_df
+
+X = Xdummies_df.values
+Y = Ydummies_df.values
+
+porcentagem_treino = 0.9
+
+tamanho_de_treino = int(porcentagem_treino * len(Y))
+tamanho_de_teste = len(Y) - tamanho_de_treino
+
+treino_dados = X[:tamanho_de_treino]
+treino_marcacoes = Y[:tamanho_de_treino]
+
+teste_dados = X[-tamanho_de_teste:]
+teste_marcacoes = Y[-tamanho_de_teste:]
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
+
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+acerto_de_um = sum(Y)
+acerto_de_zero = len(Y) - acerto_de_um
+taxa_de_acerto_base = 100.0 * max(acerto_de_um, acerto_de_zero) / len(Y)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
+# restante do código
+print("Taxa de acerto do algoritmo: %f" % taxa_de_acerto)
