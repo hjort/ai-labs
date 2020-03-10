d22f5590f33715f7dbd02c1805ddc575856db8d1
Rodrigo Hjort <rodrigo.hjort@serpro.gov.br>
2020-03-10 18:07:08 -0300

    aula01-modulo06

--- alura/machine-learning-classificacao-ii/situacao_do_cliente.py
+++ alura/machine-learning-classificacao-ii/situacao_do_cliente.py
@@ -31,34 +31,24 @@ validacao_marcacoes = Y[fim_de_treino:]
 
 
 def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
-    modelo.fit(treino_dados, treino_marcacoes)
-
-    resultado = modelo.predict(teste_dados)
-
-    acertos = resultado == teste_marcacoes
-
-    total_de_acertos = sum(acertos)
-    total_de_elementos = len(teste_dados)
-
-    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
-
-    msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_de_acerto)
-
-    print(msg)
-    return taxa_de_acerto
+  modelo.fit(treino_dados, treino_marcacoes)
+  resultado = modelo.predict(teste_dados)
+  acertos = resultado == teste_marcacoes
+  total_de_acertos = sum(acertos)
+  total_de_elementos = len(teste_dados)
+  taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+  msg = "Taxa de acerto do algoritmo {0}: {1}".format(nome, taxa_de_acerto)
+  print(msg)
+  return taxa_de_acerto
 
 def teste_real(modelo, validacao_dados, validacao_marcacoes):
-    resultado = modelo.predict(validacao_dados)
-
-    acertos = resultado == validacao_marcacoes
-
-    total_de_acertos = sum(acertos)
-    total_de_elementos = len(validacao_marcacoes)
-
-    taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
-
-    msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(taxa_de_acerto)
-    print(msg)
+  resultado = modelo.predict(validacao_dados)
+  acertos = resultado == validacao_marcacoes
+  total_de_acertos = sum(acertos)
+  total_de_elementos = len(validacao_marcacoes)
+  taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+  msg = "Taxa de acerto do vencedor entre os dois algoritmos no mundo real: {0}".format(taxa_de_acerto)
+  print(msg)
 
 resultados = {}
 
@@ -90,14 +80,15 @@ resultados[resultadoAdaBoost] = modeloAdaBoost
 maximo = max(resultados)
 vencedor = resultados[maximo]
 
-print("Vencerdor: ")
+print("Vencedor: ")
 print(vencedor)
 
 teste_real(vencedor, validacao_dados, validacao_marcacoes)
 
-acerto_base = max(Counter(validacao_marcacoes).itervalues())
+acerto_base = max(Counter(validacao_marcacoes).values())
 taxa_de_acerto_base = 100.0 * acerto_base / len(validacao_marcacoes)
 print("Taxa de acerto base: %f" % taxa_de_acerto_base)
 
 total_de_elementos = len(validacao_dados)
 print("Total de teste: %d" % total_de_elementos)
+
